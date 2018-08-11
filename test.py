0a01775e36e09c47012641ff37f77fba988acdd3
Suhrab Kurbanov <sir.suhrab@gmail.com>
2018-08-11 03:29:27 -0500

    removes unrelated test.py file

--- test.py
+++
@@ -1,8 +0,0 @@
-from BulkImageResizer.BulkImageResizer import BulkImageResizer
-
-sourcePath = '/Users/SK/Desktop/sourceFolder/'
-exportPath = '/Users/SK/Desktop/newFolder/'
-
-test1 = BulkImageResizer(sourcePath, exportPath)
-
-test1.imageResize(600, 400, aspectRatio=True)
